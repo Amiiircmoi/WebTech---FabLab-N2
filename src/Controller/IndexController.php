1f822cf261f437d76f3e9c5336096572fecd34a4
Amir <amiiir@gmail.com>
2023-03-19 16:53:10 +0100

    Edit home page html/css/js
    some edit & remove useless code

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactFormRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,12 +13,6 @@ class IndexController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('index.html.twig', []);
-    }
-
-    #[Route('/form', name: 'app_form')]
-    public function form(): Response
-    {
-        return $this->render('form.html.twig', []);
+        return $this->render('public/index.html.twig', []);
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('bymebybo', './assets/js/custom.js')
 
     .addStyleEntry('global', './assets/styles/app.scss')
 
